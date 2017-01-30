63c69b8363d0738a666a5b2f0c9212d6d81b8b9d
堀江 卓弘 <vague0110@gmail.com>
2017-01-30 18:19:03 +0900

    no message

--- main.py
+++ main.py
@@ -5,14 +5,11 @@ if __name__ == '__main__':
 	corpora = glob.glob("corpora/meiroku/*.xml")
 	for corpus in corpora:
 		M6Tagger = Tagger("surface", corpus)
-		## コーパスから語・品詞・外来語IOタグ
+		## get attributes from corpus
 		M6Tagger.makeWordPosLoanIO()
-		## LoanBI
 		M6Tagger.makeCharLoanBIESO()
-		## 2-gramデータ
 		M6Tagger.makeCharBigram()
-		## Orthoデータ
 		M6Tagger.makeCharOrtho()
-		## 結果をプリント
-		# M6Tagger.printMergeList()
-		print(M6Tagger.getCharBigram())
+
+		## print result
+		M6Tagger.printMergeList()
